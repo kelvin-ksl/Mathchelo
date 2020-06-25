4b5a5c0fb739fe298f677251fa973034de05ea28
kelvin-ksl <kelvin.ksl@hotmail.com>
2020-06-25 04:43:24 -0300

    V 1.1

---
+++ app/src/main/java/com/example/matcheloacademy/ItemListView.java
@@ -0,0 +1,32 @@
+package com.example.matcheloacademy;
+
+public class ItemListView {
+
+    private String texto;
+    private int iconeRid;
+
+    public ItemListView() {
+        this("", -1);
+    }
+
+    public ItemListView(String texto, int iconeRid) {
+        this.texto = texto;
+        this.iconeRid = iconeRid;
+    }
+
+    public int getIconeRid() {
+        return iconeRid;
+    }
+
+    public void setIconeRid(int iconeRid) {
+        this.iconeRid = iconeRid;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
--- app/src/main/java/com/example/matcheloacademy/MainActivity.java
+++ app/src/main/java/com/example/matcheloacademy/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.matcheloacademy;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -19,10 +11,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-
-    String[] fruitNames = {"Algebra","Orange","Kiwi","Melancia","Banana"};
-    int[] fruitImages = {R.drawable.algebra,R.drawable.oranges,R.drawable.kiwi,R.drawable.watermelon,R.drawable.banana};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,55 +24,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-
-
-        //finding listview
-        listView = findViewById(R.id.listview);
-
-        CustomAdapter customAdapter = new CustomAdapter();
-        listView.setAdapter(customAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Toast.makeText(getApplicationContext(),fruitNames[i],Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getApplicationContext(),listdata.class);
-                intent.putExtra("name",fruitNames[i]);
-                intent.putExtra("image",fruitImages[i]);
-                startActivity(intent);
-
-            }
-        });
-
-
-    }
-
-    private class CustomAdapter extends BaseAdapter{
-        @Override
-        public int getCount() {
-            return fruitImages.length;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            View view1 = getLayoutInflater().inflate(R.layout.activity_listdata,null);
-            //getting view in row_data
-            TextView name = view1.findViewById(R.id.fruits);
-            ImageView image = view1.findViewById(R.id.images);
-
-            name.setText(fruitNames[i]);
-            image.setImageResource(fruitImages[i]);
-            return view1;
-        }
     }
 }
